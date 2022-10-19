75437734103c9b2df4c95b1d2a969896f01ad635
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-18 22:59:54 -0400

    Dev: Requisito 6

--- helpers/saveCartItems.js
+++ helpers/saveCartItems.js
@@ -1,5 +1,8 @@
-const saveCartItems = () => {
-  // seu código aqui
+const saveCartItems = (items) => {
+  if (!items) {
+    throw new Error('Passe algum parâmetro');
+  }
+  localStorage.setItem('cartItems', items);
 };
 
 if (typeof module !== 'undefined') {
--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -4,37 +4,36 @@ const saveCartItems = require('../helpers/saveCartItems');
 localStorageSimulator('setItem');
 
 describe('3 - Teste a função saveCartItems', () => {
-  it('Testa se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado',() => {
-    
-    expect(localStorage.getItem).toHaveBeenCalled()
+
+  it('Testa se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado', () => {
+
+    expect.assertions( 1 );
+    saveCartItems('item aqui');
+    expect(localStorage.setItem).toHaveBeenCalled();
+
   })
-  
-  it('Testa se, ao executar saveCartItems com um cartItem como argumento, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems.',() => {
-    const cartItem = ''
-    expect()
+  it('Testa se, o método localStorage.setItem é chamado com dois parâmetros, sendo o primeiro a chave \'cartItems\' e o segundo sendo o valor passado como argumento para saveCartItems.', () => {
+    
+    expect.assertions( 1 );
+    saveCartItems( 'item aqui' );
+    const key = 'cartItems';
+    const value = 'item aqui'
+
+    expect( localStorage.setItem ).toHaveBeenLastCalledWith( key, value )
+
+  })
+  it('testa se a função saveCartItems existe', () => {
+
+    expect.assertions( 1 );
+    expect( typeof saveCartItems ).toBe( 'function' );
+
+  })
+  it('testa se um erro é lançado quando o parâmetro está vazio ou sem argumento', () => {
+
+    expect.assertions( 2 );
+    expect( () => saveCartItems() ).toThrow( 'Passe algum parâmetro' );
+    expect( () => saveCartItems() ).toThrow( Error );
+
   })
 
-  it('testa se a função saveCartItems existe',() => {
-    
-  })
-
-  it('testa se o fetch é chamado na função saveCartItems',() => {
-    
-  })  
-  
-  it('',() => {
-    
-  })  
-  
-  it('',() => {
-    
-  })  
-  
-  it('',() => {
-    
-  })  
-  
-  it('',() => {
-    
-  })
 });
