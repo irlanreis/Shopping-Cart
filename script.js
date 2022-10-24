782c3887147ebed080e18d9f5298b0277e692e86
Irlan Dos Santos Reis <xuiexpress@gmail.com>
2022-10-24 14:24:48 -0400

    Dev: Corrige linter

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Esse tipo de comentário que estão antes de todas as funções são chamados de JSdoc,
 // experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições
 // Fique a vontade para modificar o código já escrito e criar suas próprias funções!
-const olCartItems = document.querySelector('.cart__items');
-
 /**
  * Função responsável por criar e retornar o elemento de imagem do produto.
  * @param {string} imageSource - URL da imagem.
